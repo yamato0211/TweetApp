42a781d67e1cc8277af8780881c3d48be6764fc4
yamato0211 <kumayama0211@gmail.com>
2022-03-14 07:22:37 +0900

    新規投稿機能と投稿詳細ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "desc")
   end
 
   def new
-    
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      flash.now[:notice] = "空欄での投稿もしくは400字以上の投稿はできません"
+      render("posts/new")
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post "posts/create" => "posts#create"
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
+  get "posts/:id" => "posts#show"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
