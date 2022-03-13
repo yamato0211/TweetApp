6639509568034bbc6f33c466aa6ed25cc1ad8e6c
yamato0211 <kumayama0211@gmail.com>
2022-03-14 01:59:58 +0900

    投稿一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
